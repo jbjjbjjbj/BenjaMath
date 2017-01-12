5a668faacc784da6da4cf4ce9b99de1bc0a4e801
benjadahl <benjadahl@gmail.com>
2017-01-12 17:16:38 +0100

    Made the save button work in the electron version

--- electronWrapper/electronWrapper.js
+++ electronWrapper/electronWrapper.js
@@ -1,11 +1,33 @@
 //This script will refresh the iframe contains the #content element
 //Thus the server having started
 
-var frameContent = document.getElementById("contentFrame");
+//The content of the iframe
+var frameContent = document.getElementById("contentFrame").contentWindow;
 
 setTimeout(function() {
-  let contentTest = frameContent.contentWindow.document.getElementById("content");
+  //Refresh after 500 ms if the document is not loaded
+  let contentTest = frameContent.document.getElementById("content");
   if(contentTest === null){
     location.reload();
   }
 }, 500);
+
+//When the iframe is loaded run this
+$("iframe#contentFrame").on("load", function(){
+  //Depedencies for the save function
+  var app = require("electron").remote;
+  var dialog = app.dialog;
+  var fs = require('fs');
+
+  //Overwrite the click event for the save button
+  frameContent.document.getElementById("saveButton").addEventListener("click", function(){
+    let editorData = frameContent.CKEDITOR.instances.editor.getData();
+    dialog.showSaveDialog(function(path){
+      fs.writeFile(path, editorData, function(err) {
+        if(err) {
+          return console.log(err);
+        }
+      });
+    });
+  });
+});
